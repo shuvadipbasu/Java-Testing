3cf151b05ddb07a291d1019f34522b751ad581f4
Jenkins <shuvadipengg@gmail.com>
2019-02-26 23:28:12 +0530

    First test

--- CucumberLearning/Cucumber-Part1/src/main/java/com/test/features/PageLoadFeature.feature
+++ CucumberLearning/Cucumber-Part1/src/main/java/com/test/features/PageLoadFeature.feature
@@ -2,5 +2,5 @@ Feature: Page Load Feature
 
 Scenario: User goes at login page
 
-Given user is at login page
-Then user is at login page
+Given user is at login page of github
+Then title of the page is The world’s leading software development platform · GitHub
--- CucumberLearning/Cucumber-Part1/src/main/java/com/test/steps/StepDefinition.java
+++ CucumberLearning/Cucumber-Part1/src/main/java/com/test/steps/StepDefinition.java
@@ -1,12 +1,27 @@
 package com.test.steps;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinition {
 
-    @Given("^user is at login page$")
+    WebDriver driver;
+    private static final String _DRIVER_LOC="D:\\GitRepos\\Java-Testing\\CucumberLearning\\Cucumber-Part1\\src\\main\\resources\\driver\\chromedriver.exe";
+
+    @Given("^user is at login page of github$")
     public void userOnSite(){
-        Assert.assertEquals(1.0,1.0,0);
+        System.setProperty("webdriver.chrome.driver",_DRIVER_LOC);
+         driver = new ChromeDriver();
+         driver.get("https://github.com/");
     }
+
+    @Then("^title of the page is The world’s leading software development platform · GitHub$")
+    public void checkTitleOfThePage(){
+       Assert.assertEquals("The world’s leading software development platform · GitHub",driver.getTitle());
+       driver.close();
+    }
+
 }
